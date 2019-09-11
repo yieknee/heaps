079569622cc65354215ca192595863d4da78020d
CheezItMan <chris@adadevelopersacademy.org>
2019-09-11 15:12:05 -0700

    test_helper

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+  
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require "minitest/skip_dsl"
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative '../lib/heap'
+require_relative '../lib/heap_sort'
