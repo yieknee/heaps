e572c2143319cac9fac8dee16442d592404a7e7a
CheezItMan <chris@adadevelopersacademy.org>
2020-09-27 12:24:36 -0700

    heap_sort to heapsort

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -3,6 +3,6 @@
 # This method uses a heap to sort an array.
 # Time Complexity:  ?
 # Space Complexity: ?
-def heap_sort(list)
+def heapsort(list)
   raise NotImplementedError, "Method not implemented yet..."
 end
